88231180534ee8fa215f160cf86a8ef2b39b66e2
Shreyash Patil <107754587+shreyashap@users.noreply.github.com>
2025-12-26 18:07:18 +0530

    Merge pull request #2 from shreyashap/feat/backend-login
    feat(backend): Completed login API

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,4 +5,8 @@ dotenv.config();
 export const env = {
     nodeEnv: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
+    accessTokenSecret : process.env.ACCESS_TOKEN_SECRET,
+    refreshTokenSecret : process.env.REFRESH_TOKEN_SECRET,
+    accessTokenExpiry : process.env.ACCESS_TOKEN_EXPIRY,
+    refreshTokenExpiry : process.env.REFRESH_TOKEN_EXPIRY
 }
--- backend/src/modules/auth/controllers/auth.controller.ts
+++ backend/src/modules/auth/controllers/auth.controller.ts
@@ -4,6 +4,7 @@ import { authService } from "../services/auth.service";
 import { hashPassword } from "../../utils";
 import { asyncHandler } from "../../../shared/utils/async-handler";
 import { RegisterResponse } from "../../types";
+import { env } from "../../../config";
 
 class AuthController {
     registerUser = asyncHandler (async (req:Request,res:Response<{data : RegisterResponse,message : string}>) => {
@@ -33,6 +34,31 @@ class AuthController {
                 role: user.role
             }
         });
+    })
+
+    loginUser = asyncHandler(async(req:Request,res:Response)=>{
+        const {email,password } = req.body;
+
+        if(!email || !password){
+            throw new AppError('All fields are required',400);
+        }
+
+        const {userData,accessToken,refreshToken}  = await authService.login({email,password});
+
+        const nodeEnv = env.nodeEnv;
+        const options = {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            path: '/auth/refresh'
+        }
+
+
+        res.status(200).cookie('refreshToken',refreshToken).json({
+            message : 'User logged in successfully',
+            data : userData,
+            accessToken
+        })
     })
     
 }
