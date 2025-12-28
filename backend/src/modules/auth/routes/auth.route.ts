831f4f216557220be4376f0c827b7723954733c8
Shreyash Patil <107754587+shreyashap@users.noreply.github.com>
2025-12-28 21:36:16 +0530

    complete logout backend (#3)
    Co-authored-by: Shreyash <shreyashap>

--- backend/src/modules/auth/routes/auth.route.ts
+++ backend/src/modules/auth/routes/auth.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { authController } from "../controllers/auth.controller";
+import { verifyJWT, verifyRefreshToken } from "../../../shared/middleware/auth.middleware";
 
 const authRouter = Router();
 
 authRouter.route('/register').post(authController.registerUser);
 authRouter.route('/login').post(authController.loginUser);
+authRouter.route("/logout").post(verifyJWT,authController.logoutUser);
+authRouter.route("/refresh/token").get(verifyRefreshToken,authController.refreshAccessToken);
 
 export  {authRouter};
