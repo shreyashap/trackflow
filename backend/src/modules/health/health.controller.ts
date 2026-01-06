bfe6d9b38d6ab7d3dbb12d18eb5f4156b0f779d8
Shreyash Patil <107754587+shreyashap@users.noreply.github.com>
2026-01-06 23:32:06 +0530

    add success to health api (#7)
    Co-authored-by: Shreyash <shreyashap>

--- backend/src/modules/health/health.controller.ts
+++ backend/src/modules/health/health.controller.ts
@@ -1,9 +1,8 @@
-import { Request, Response } from "express";
-
-
+import type { Request, Response } from "express";
 
 export const healthCheck = (_req: Request, res: Response) => {
-    res.status(200).json({
+  res.status(200).json({
+    success: true,
     status: "ok",
     timestamp: new Date().toISOString(),
   });
